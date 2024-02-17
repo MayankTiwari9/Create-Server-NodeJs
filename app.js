8d3c4c1513b4246cf205e20636308bcd7d13380e
MayankTiwari9 <mt1027319@gmail.com>
2024-02-17 13:38:05 +0530

    New Input for size added

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const http = require("http");
-
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST">' +
+    '<input type="text" name="title" placeholder="Product Title"/>' +
+    '<input type="text" name="size" placeholder="Product Size"/>' +
+    '<button type="submit">Add Product</button>' +
+    '</form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express!</h1>');
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-
-const server = http.createServer(app);
-
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
 });
+
+app.listen(4000);
