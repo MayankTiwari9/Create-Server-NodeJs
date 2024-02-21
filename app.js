430126468580301ce150308d754d2576ae3407c3
MayankTiwari9 <mt1027319@gmail.com>
2024-02-21 19:39:31 +0530

    Serving html css js files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -5,14 +6,21 @@ const app = express();
 
 const adminRouter = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactUs = require('./routes/contact-us');
+const success = require('./routes/success');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRoutes);
+app.use(contactUs);
+app.use(success);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
-app.listen(4000);
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
